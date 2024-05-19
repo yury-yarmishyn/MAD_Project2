d4890595bc3b9b679730d6aa0c3f6ca5956569cb
Stan-Nip <84511960+Stan-Nip@users.noreply.github.com>
2024-05-19 15:08:01 +0200

    0.1
    -Added data to code

--- main.py
+++ main.py
@@ -1,16 +1,65 @@
-# This is a sample Python script.
+class Track:
+    def __init__(self, name, artist, genre, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.danceability = self._convert_to_float(danceability)
+        self.energy = self._convert_to_float(energy)
+        self.key = self._convert_to_int(key)
+        self.loudness = self._convert_to_float(loudness)
+        self.mode = self._convert_to_int(mode)
+        self.speechiness = self._convert_to_float(speechiness)
+        self.acousticness = self._convert_to_float(acousticness)
+        self.instrumentalness = self._convert_to_float(instrumentalness)
+        self.liveness = self._convert_to_float(liveness)
+        self.valence = self._convert_to_float(valence)
+        self.tempo = self._convert_to_float(tempo)
+        self.duration = self._convert_to_int(duration)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def _convert_to_float(self, value):
+        try:
+            return float(value)
+        except ValueError:
+            return None
 
+    def _convert_to_int(self, value):
+        try:
+            return int(value)
+        except ValueError:
+            return None
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def __repr__(self):
+        return (f"Track(name={self.name}, artist={self.artist}, genre={self.genre}, danceability={self.danceability}, "
+                f"energy={self.energy}, key={self.key}, loudness={self.loudness}, mode={self.mode}, speechiness={self.speechiness}, "
+                f"acousticness={self.acousticness}, instrumentalness={self.instrumentalness}, liveness={self.liveness}, "
+                f"valence={self.valence}, tempo={self.tempo}, duration={self.duration})")
 
+def create_track_list(file_path):
+    track_list = []
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            data = line.strip().split(',')
+            track = Track(
+                name=data[1],
+                artist=data[2],
+                genre=data[9],
+                danceability=data[11],
+                energy=data[12],
+                key=data[13],
+                loudness=data[14],
+                mode=data[15],
+                speechiness=data[16],
+                acousticness=data[17],
+                instrumentalness=data[18],
+                liveness=data[19],
+                valence=data[20],
+                tempo=data[21],
+                duration=data[22]
+            )
+            track_list.append(track)
+    return track_list
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+track_list = create_track_list('Tracks.txt')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for track in track_list[:10]:
+    print(track)
